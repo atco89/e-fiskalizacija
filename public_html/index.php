a76e4d5dd5092c303399710a1127ed45df8a7a12
SWIFTSOFT <git@swiftsoft.rs>
2022-05-09 18:21:09 +0200

    Work in progress.

--- public_html/index.php
+++ public_html/index.php
@@ -4,18 +4,6 @@ declare(strict_types=1);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use TaxCore\Examples\Configuration;
-use TaxCore\Examples\Request\AdvanceSale;
-use TaxCore\Examples\Request\AdvanceSaleBuyerIdentified;
-use TaxCore\Examples\Request\AdvanceSaleBuyerIdentifiedRefund;
-use TaxCore\Examples\Request\AdvanceSaleRefund;
-use TaxCore\Examples\Request\CopySale;
-use TaxCore\Examples\Request\CopySaleBuyerIdentified;
-use TaxCore\Examples\Request\CopySaleRefund;
-use TaxCore\Examples\Request\CopySaleRefundBuyerIdentified;
-use TaxCore\Examples\Request\NormalSale;
-use TaxCore\Examples\Request\NormalSaleBuyerAndCostCenterIdentified;
-use TaxCore\Examples\Request\NormalSaleBuyerIdentifiedRefund;
-use TaxCore\Examples\Request\NormalSaleRefund;
 use TaxCore\Request;
 use TaxCore\Response\ResponseBuilder;
 use TaxCore\Response\ResponsesBuilder;
@@ -25,57 +13,113 @@ try {
 
     // ===== NORMAL SALE =====
 
-    $n1 = $request->normalSale(new NormalSale());
+    $n1 = $request->normalSale('');
     saveOne($n1);
 
-    $n2 = $request->normalSaleBuyerAndCostCenterIdentified(new NormalSaleBuyerAndCostCenterIdentified());
+    $n2 = $request->normalSaleBuyerAndCostCenterIdentified(
+        '',
+        '',
+        ''
+    );
     saveOne($n2);
 
-    $n3 = $request->normalSaleRefund(new NormalSaleRefund($n1->getResponse()));
+    $n3 = $request->normalSaleRefund(
+        '',
+        '',
+        ''
+    );
     saveOne($n3);
 
-    $n4 = $request->normalSaleBuyerIdentifiedRefund(new NormalSaleBuyerIdentifiedRefund($n2->getResponse()));
+    $n4 = $request->normalSaleBuyerIdentifiedRefund(
+        '',
+        '',
+        '',
+        ''
+    );
     saveOne($n4);
 
     // ===== ADVANCE SALE =====
 
-    $a1 = $request->advanceSale(new AdvanceSale());
+    $a1 = $request->advanceSale(
+        '',
+        ''
+    );
     saveOne($a1);
 
-    $a2 = $request->advanceSaleBuyerIdentified(new AdvanceSaleBuyerIdentified());
+    $a2 = $request->advanceSaleBuyerIdentified(
+        '',
+        '',
+        ''
+    );
     saveOne($a2);
 
-    $a3 = $request->advanceSaleRefund(new AdvanceSaleRefund($a1->getResponse()));
+    $a3 = $request->advanceSaleRefund(
+        '',
+        '',
+        '',
+        ''
+    );
     saveOne($a3);
 
-    $a4 = $request->advanceSaleBuyerIdentifiedRefund(new AdvanceSaleBuyerIdentifiedRefund($a2->getResponse()));
+    $a4 = $request->advanceSaleBuyerIdentifiedRefund(
+        '',
+        '',
+        '',
+        '',
+        ''
+    );
     saveOne($a4);
 
     // ===== CLOSE ADVANCE SALE =====
 
     $n5 = $request->normalSaleWithClosingAdvanceSale(
-        new AdvanceSaleRefund($a1->getResponse()),
-        new NormalSale()
+        '',
+        '',
+        '',
+        ''
     );
     saveAll($n5);
 
     $n6 = $request->normalSaleBuyerIdentifiedWithClosingAdvanceSale(
-        new AdvanceSaleBuyerIdentifiedRefund($a2->getResponse()),
-        new NormalSaleBuyerAndCostCenterIdentified());
+        '',
+        '',
+        '',
+        '',
+        ''
+    );
     saveAll($n6);
 
     // ===== COPY SALE =====
 
-    $c1 = $request->copySale(new CopySale($n1->getResponse()));
+    $c1 = $request->copySale(
+        '',
+        '',
+        ''
+    );
     saveOne($c1);
 
-    $c2 = $request->copySaleBuyerIdentifiedBuilder(new CopySaleBuyerIdentified($n2->getResponse()));
+    $c2 = $request->copySaleBuyerIdentifiedBuilder(
+        '',
+        '',
+        '',
+        ''
+    );
     saveOne($c2);
 
-    $c3 = $request->copySaleRefund(new CopySaleRefund($n3->getResponse()));
+    $c3 = $request->copySaleRefund(
+        '',
+        '',
+        '',
+        ''
+    );
     saveOne($c3);
 
-    $c4 = $request->copySaleBuyerIdentifiedRefund(new CopySaleRefundBuyerIdentified($n4->getResponse()));
+    $c4 = $request->copySaleBuyerIdentifiedRefund(
+        '',
+        '',
+        '',
+        ''
+    );
     saveOne($c4);
 } catch (Exception $e) {
     die($e->getMessage());
